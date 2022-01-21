712cd29b12539d400baa9f9a3d0fb1726ac0791b
IMEGHRI Sami <IMEGHRISAMI@gmail.com>
2022-01-21 06:27:18 +0100

    :tada: init

---
+++ bmma-app-server/src/main/java/com/bmma/bmmabackend/BmmabackendApplication.java
@@ -0,0 +1,13 @@
+package com.bmma.bmmabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmmabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmmabackendApplication.class, args);
+	}
+
+}
---
+++ bmma-app-server/src/test/java/com/bmma/bmmabackend/BmmabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmma.bmmabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmmabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
