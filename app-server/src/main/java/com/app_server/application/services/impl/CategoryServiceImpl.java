6d7f4d88c47b6be1423b96faf664138ed09ba3cf
Zin Eddine Lamziouik <zineddinelamziouik@gmail.com>
2022-02-25 23:12:13 +0100

    added categories access

--- app-server/src/main/java/com/app_server/application/services/impl/CategoryServiceImpl.java
+++ app-server/src/main/java/com/app_server/application/services/impl/CategoryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.app_server.application.services.impl;
 
+import com.app_server.application.models.Category;
 import com.app_server.application.repositories.CategoryRepository;
 import com.app_server.application.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -11,4 +14,8 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Override
+    public List<Category> getCategories(){
+        return categoryRepository.findAll();
+    }
 }
